d941351181d3c45e2a5a437268ee50ff5201e099
RahniDev <rahni32@protonmail.com>
2024-08-21 07:48:37 +0200

    Create sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
+import Sidebar from "@/components/Sidebar"
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Rahni', lastName: 'De-Meis'}
   return (
-   <main>SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar />
     {children}
    </main>
   );
