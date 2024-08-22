950f1a579c16a6964a3e466715af82695ae8773d
RahniDev <rahni32@protonmail.com>
2024-08-22 17:43:18 +0200

    Build sidebar and bank cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,32 @@
 
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Rahni'}
+  const loggedIn = { firstName: 'Rahni', lastName: 'De-Meis',
+    email: 'contact@gmail.com'
+   }
   return (
-    <section className="home"> 
-    <div className="home-content">
-      <header className="home-header">
-       <HeaderBox type="greeting" 
-       title="Welcome"
-       user={loggedIn.firstName || 'Guest'} 
-       subtext="Manage your account and transactions efficiently."/>
-      <TotalBalanceBox 
-      accounts={[]}
-      totalBanks={1}
-      totalCurrentBalance={1490.21}/>
-      </header>
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || 'Guest'}
+            subtext="Manage your account and transactions efficiently." />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1490.21} />
+        </header>
+        {/* Recent transactions */}
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 108}, {currentBalance: 206}]}
+      />
     </section>
   )
 }
