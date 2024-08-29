f7d7219b9d2a63c6213674c96eefb2e054b283c0
RahniDev <rahni32@protonmail.com>
2024-08-29 13:19:22 +0200

    Complete user sign in

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,18 +2,20 @@
 
 import { ID } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite";
-import {cookies} from 'next/headers'
+import { cookies } from 'next/headers'
 import { parseStringify } from "../utils";
-export const signIn = async () => {
+export const signIn = async ({email, password}: signInProps) => {
     try {
-        // Mutation / Database / Make fetch request
+        const { account } = await createAdminClient();
+        const response = await account.createEmailPasswordSession(email, password)
+        return parseStringify(response)
     } catch (error) {
         console.error('Error', error)
     }
 }
 
 export const signUp = async (userData: SignUpParams) => {
-    const {email, password, firstName, lastName} = userData
+    const { email, password, firstName, lastName } = userData
     try {
         const { account } = await createAdminClient();
 
@@ -21,8 +23,11 @@ export const signUp = async (userData: SignUpParams) => {
             email,
             password,
             `${firstName} ${lastName}`);
-            
-        const session = await account.createEmailPasswordSession(email, password);
+
+        const session = await account.createEmailPasswordSession(
+            email, 
+            password
+        );
 
         cookies().set("appwrite-session", session.secret, {
             path: "/",
@@ -39,7 +44,8 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
     try {
         const { account } = await createSessionClient();
-        return await account.get();
+        const user = await account.get();
+        return parseStringify(user)
     } catch (error) {
         return null;
     }
